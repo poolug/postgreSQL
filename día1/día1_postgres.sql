c8734689284ab7013f237c453cce02a9efc2d1db
poolug <pauluzcategui43@gmail.com>
2021-06-09 21:03:45 -0400

    update file

--- día1/día1_postgres.sql
+++ día1/día1_postgres.sql
@@ -4,24 +4,24 @@ CREATE DATABASE posts;
 
 \c posts;
 
-CREATE TABLE post(id SERIAL UNIQUE,
+CREATE TABLE post(id INT,
 nombre_usuario VARCHAR(20),
 fecha_creacion TIMESTAMP,
 contenido VARCHAR(50),
 descripcion VARCHAR(50),
-PRIMARY KEY (contenido));
+PRIMARY KEY (id));
 
-INSERT INTO post (nombre_usuario, fecha_creacion, contenido, descripcion)
+INSERT INTO post (id, nombre_usuario, fecha_creacion, contenido, descripcion)
 VALUES
-('Pamela', '2021-06-08', 'Post de Pamela', 'Descripcion post1 Pamela');
+(1, 'Pamela', '2021-06-08', '1Post de Pamela', 'Descripcion post1 Pamela');
 
-INSERT INTO post (nombre_usuario, fecha_creacion, contenido, descripcion)
+INSERT INTO post (id, nombre_usuario, fecha_creacion, contenido, descripcion)
 VALUES
-('Pamela', '2021-06-09', 'Post de Pamela', 'Descripcion post2 Pamela');
+(2, 'Pamela', '2021-06-09', '2Post de Pamela', 'Descripcion post2 Pamela');
 
-INSERT INTO post (nombre_usuario, fecha_creacion, contenido, descripcion)
+INSERT INTO post (id, nombre_usuario, fecha_creacion, contenido, descripcion)
 VALUES
-('Carlos', '2021-06-10', 'Post de Carlos', 'Descripcion post1 Carlos');
+(3, 'Carlos', '2021-06-10', '1Post de Carlos', 'Descripcion post1 Carlos');
 
 ALTER TABLE post
 ADD titulo VARCHAR(25);
@@ -35,65 +35,65 @@ WHERE id = 2;
 UPDATE post SET titulo = 'titulo1 Carlos'
 WHERE id = 3;
 
-INSERT INTO post (nombre_usuario, fecha_creacion, contenido, descripcion)
+INSERT INTO post (id, nombre_usuario, fecha_creacion, contenido, descripcion)
 VALUES
-('Pedro', '2021-06-11', 'Post de Pedro', 'Descripcion post1 Pedro');
+(4, 'Pedro', '2021-06-11', 'Post de Pedro', 'Descripcion post1 Pedro');
 
 DELETE FROM post WHERE nombre_usuario = 'Carlos';
 
-INSERT INTO post (nombre_usuario, fecha_creacion, contenido, descripcion)
+INSERT INTO post (id, nombre_usuario, fecha_creacion, contenido, descripcion)
 VALUES
-('Carlos', '2021-06-12', 'Post de Carlos', 'Descripcion post2 Carlos');
+(5, 'Carlos', '2021-06-12', 'Post de Carlos', 'Descripcion post2 Carlos');
 
-CREATE TABLE comentarios(id SERIAL UNIQUE,
+CREATE TABLE comentarios(id INT,
 hora_de_creacion TIME,
 contenido VARCHAR(50),
-FOREIGN KEY (contenido) REFERENCES post(contenido));
+FOREIGN KEY (id) REFERENCES post(id));
 
-INSERT INTO comentarios (hora_de_creacion, contenido)
+INSERT INTO comentarios (id, hora_de_creacion, contenido)
 VALUES
-('10:25', 'Post de Pamela');
+(1, '10:25', '1Comentario Post de Pamela');
 
-INSERT INTO comentarios (hora_de_creacion, contenido)
+INSERT INTO comentarios (id, hora_de_creacion, contenido)
 VALUES
-('11:25', 'Post de Pamela');
+(1, '11:25', '2Comentario Post de Pamela');
 
-INSERT INTO comentarios (hora_de_creacion, contenido)
+INSERT INTO comentarios (id, hora_de_creacion, contenido)
 VALUES
-('12:25', 'Post de Carlos');
+(4, '12:25', 'Post de Carlos');
 
-INSERT INTO comentarios (hora_de_creacion, contenido)
+INSERT INTO comentarios (id, hora_de_creacion, contenido)
 VALUES
-('13:25', 'Post de Carlos');
+(4, '13:25', 'Post de Carlos');
 
-INSERT INTO comentarios (hora_de_creacion, contenido)
+INSERT INTO comentarios (id, hora_de_creacion, contenido)
 VALUES
-('14:25', 'Post de Carlos');
+(4, '14:25', 'Post de Carlos');
 
-INSERT INTO comentarios (hora_de_creacion, contenido)
+INSERT INTO comentarios (id, hora_de_creacion, contenido)
 VALUES
-('15:25', 'Post de Carlos');
+(4, '15:25', 'Post de Carlos');
 
-INSERT INTO post (nombre_usuario, fecha_creacion, contenido, descripcion)
+INSERT INTO post (id, nombre_usuario, fecha_creacion, contenido, descripcion)
 VALUES
-('Margarita', '2021-08-11', 'Post de Margarita', 'Descripcion post1 Margarita');
+(6, 'Margarita', '2021-08-11', 'Post de Margarita', 'Descripcion post1 Margarita');
 
-INSERT INTO comentarios (hora_de_creacion, contenido)
+INSERT INTO comentarios (id, hora_de_creacion, contenido)
 VALUES
-('16:25', 'Post de Margarita');
+(6, '16:25', '1Comentario de Margarita');
 
-INSERT INTO comentarios (hora_de_creacion, contenido)
+INSERT INTO comentarios (id, hora_de_creacion, contenido)
 VALUES
-('16:45', 'Post de Margarita');
+(6, '16:45', '2Comentario de Margarita');
 
-INSERT INTO comentarios (hora_de_creacion, contenido)
+INSERT INTO comentarios (id, hora_de_creacion, contenido)
 VALUES
-('17:00', 'Post de Margarita');
+(6, '17:00', '3Comentario de Margarita');
 
-INSERT INTO comentarios (hora_de_creacion, contenido)
+INSERT INTO comentarios (id, hora_de_creacion, contenido)
 VALUES
-('17:25', 'Post de Margarita');
+(6, '17:25', '4Comentario de Margarita');
 
-INSERT INTO comentarios (hora_de_creacion, contenido)
+INSERT INTO comentarios (id, hora_de_creacion, contenido)
 VALUES
-('18:25', 'Post de Margarita');
+(6, '18:25', '5Comentario de Margarita');
