c8b6335d2240593ffeedf009c5ba685c83864970
poolug <pauluzcategui43@gmail.com>
2021-06-20 16:09:37 -0400

    update día4

--- día4/a.sql
+++ día4/a.sql
@@ -1,19 +1,20 @@
 -- Desactivado el autocommit
 \set AUTOCOMMIT off
 
--- Inicia la transacción para
+-- Inicia la transacción para la compra del usuario01
 BEGIN;
 
--- Inserta la compra del usuario01
+-- Inserta la compra
 INSERT INTO compra (cliente_id, fecha)
 VALUES (1, NOW());
 
+-- Inserta el detalle de la compra
 INSERT INTO detalle_compra (producto_id, compra_id, cantidad)
 VALUES (9, (SELECT MAX(id) FROM compra), 5);
 
 -- Actualiza el stock del producto9, restándole 5.
 -- Generará error, ya que el stock del producto9 es 4
-UPDATE producto SET stock = stock - 5 WHERE id = 9;
+UPDATE producto SET stock = stock -5 WHERE id = 9;
 
 -- Cierra la transacción y hace ROLLBACK automáticamente y no actualiza el stock
 COMMIT;
@@ -31,28 +32,25 @@ ORDER BY id ASC;
 --Ingresando las compras del usuario02
 BEGIN;
 SAVEPOINT check_one;
-
-UPDATE producto SET stock = stock -3 WHERE id = 1;
 INSERT INTO compra (cliente_id, fecha)
 VALUES (2, NOW());
 INSERT INTO detalle_compra (producto_id, compra_id, cantidad)
 VALUES (1, (SELECT MAX(id) FROM compra), 3);
+UPDATE producto SET stock = stock -3 WHERE id = 1;
 
 SAVEPOINT check_one;
-
-UPDATE producto SET stock = stock -3 WHERE id = 2;
 INSERT INTO compra (cliente_id, fecha)
 VALUES (2, NOW());
 INSERT INTO detalle_compra (producto_id, compra_id, cantidad)
 VALUES (2, (SELECT MAX(id) FROM compra), 3);
+UPDATE producto SET stock = stock -3 WHERE id = 2;
 
 SAVEPOINT check_one;
-
-UPDATE producto SET stock = stock - 3 WHERE id = 8;
 INSERT INTO compra (cliente_id, fecha)
 VALUES (2, NOW());
 INSERT INTO detalle_compra (producto_id, compra_id, cantidad)
 VALUES (8, (SELECT MAX(id) FROM compra), 3);
+UPDATE producto SET stock = stock -3 WHERE id = 8;
 
 ROLLBACK TO check_one;
 
@@ -69,7 +67,7 @@ ORDER BY id ASC;
 
 BEGIN;
 INSERT INTO cliente (nombre, email)
-VALUES ('Usuario011', 'usuario11@gmail.com');
+VALUES ('Usuario011', 'usuario011@gmail.com');
 SELECT * FROM cliente ORDER BY id ASC;
 ROLLBACK;
 SELECT * FROM cliente ORDER BY id ASC;
